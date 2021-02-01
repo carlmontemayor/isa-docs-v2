3f64f409b6917dc9ebd8d0c2431b32a6951a0177
ayanakano <ayanakano26@gmail.com>
2021-02-01 15:14:08 -0800

    Fixed linking bug from doc page to doc page

--- pages/en/users.js
+++ pages/en/users.js
@@ -5,9 +5,9 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-const React = require('react');
+const React = require("react");
 
-const CompLibrary = require('../../core/CompLibrary.js');
+const CompLibrary = require("../../core/CompLibrary.js");
 
 const Container = CompLibrary.Container;
 
@@ -19,7 +19,7 @@ class Users extends React.Component {
     }
 
     const editUrl = `${siteConfig.repoUrl}/edit/master/website/siteConfig.js`;
-    const showcase = siteConfig.users.map(user => (
+    const showcase = siteConfig.users.map((user) => (
       <a href={user.infoLink} key={user.infoLink}>
         <img src={user.image} alt={user.caption} title={user.caption} />
       </a>
@@ -27,7 +27,7 @@ class Users extends React.Component {
 
     return (
       <div className="mainContainer">
-        <Container padding={['bottom', 'top']}>
+        <Container padding={["bottom", "top"]}>
           <div className="showcaseSection">
             <div className="prose">
               <h1>Who is Using This?</h1>
